d5986f899950491f2c6e5b3abcf4cdd535115f61
Tokiyashi <rogich6319@gmail.com>
2022-06-18 20:03:38 +0300

    made func to display current date

--- src/App.js
+++ src/App.js
@@ -6,6 +6,19 @@ const api = {
 }
 
 function App() {
+
+    const dateBuilder = (d) => {
+        const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
+        const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+        let day = days[d.getDay()];
+        let date = d.getDate();
+        let month = months[d.getMonth()];
+        let year = d.getFullYear()
+
+        return `${day} ${date} ${month} ${year}`
+    }
+
   return (
     <main>
       <div className="search-box">
@@ -15,6 +28,22 @@ function App() {
           placeholder="Search..."
         />
       </div>
+        <div className="location-box">
+            <div className="location">
+              New York, US
+            </div>
+            <div className="date">
+                {dateBuilder(new Date())}
+            </div>
+        </div>
+        <div className="weather-box">
+            <div className="temperature">
+                15 °c
+            </div>
+            <div className="weather">
+                Sunny
+            </div>
+        </div>
     </main>
   );
 }
